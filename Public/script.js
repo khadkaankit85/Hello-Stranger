5e2932136bc6f3106282b8ea1e75b6f552eedd00
Angkit Khadka <khadkaankit85@gmail.com>
2024-06-19 17:36:39 -0400

    Ui Changes

--- Public/script.js
+++ Public/script.js
@@ -1,10 +1,14 @@
 const buttonToAnonymousChat = document.getElementById("linkto-anonymous-chat-page")
 const buttonToAnonymousVideoCall = document.getElementById("linkto-anonymous-video-page")
+// const messageOptionsDialogueBox = document.getElementById("dialogue-box-for-chatting-options")
 
 buttonToAnonymousVideoCall.addEventListener("click", () => {
     console.log("button clickec")
     alert("Coming Soon")
 })
 buttonToAnonymousChat.addEventListener("click", () => {
-    window.location = "/chat"
+    // window.location = "/chat"
+    console.log("clicked")
+    // messageOptionsDialogueBox.showModal()
+
 })
